a5e5fa14e967cfeebd4f79b9d20422ebdf0e7b40
Yota Toyama <raviqqe@gmail.com>
2018-08-08 17:19:17 -0700

    Add constraints of X

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,48 @@
-import { ITrip, IWaypointOutput } from "./domain";
+import {
+  Constraint,
+  Expression,
+  Operator,
+  Solver,
+  Strength,
+  Variable
+} from "kiwi.js";
+import * as lodash from "lodash";
+
+import { ILocation, ITrip, IWaypointOutput } from "./domain";
 
 export function execute(trips: ITrip[]): IWaypointOutput[] {
+  const locations: ILocation[] = trips.flatMap(
+    ({ pickupWaypoint, dropoffWaypoint }) => [
+      pickupWaypoint.location,
+      dropoffWaypoint.location
+    ]
+  );
+
+  const xss = new Array(locations.length).map(() =>
+    new Array(locations.length).map(() => new Variable())
+  );
+
+  const solver = new Solver();
+
+  for (const xs of xss) {
+    for (const x of xs) {
+      solver.createConstraint(x, Operator.Ge);
+      solver.createConstraint(x, Operator.Le, 1);
+    }
+  }
+
+  for (const xs of xss) {
+    solver.createConstraint(new Expression(...xs), Operator.Eq);
+  }
+
+  for (const xs of lodash.zip(...xss)) {
+    solver.createConstraint(new Expression(...xs), Operator.Eq);
+  }
+
+  solver.updateVariables();
+
   return [];
 }
+
+// tslint:disable no-console
+console.log(execute([]));
