a6e443039679e9c85f8b56db312e5153683bbd5d
Yota Toyama <raviqqe@gmail.com>
2018-08-09 18:09:44 -0700

    Remove javascript files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
-  testEnvironment: "node",
-  testMatch: ["<rootDir>/src/**/__tests__/*.ts"]
-};
